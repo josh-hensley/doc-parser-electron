bd3d8a2813699246edf3597ced9622f5c7cb59f0
josh-hensley <me@joshhensley.com>
2024-12-28 21:32:55 -0600

    altered to be more specifically useful, shouldn't work until you've dropped an xlsx file

--- main.js
+++ main.js
@@ -3,90 +3,270 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { parseOfficeAsync } from 'officeparser';
 import writeXlsxFile from 'write-excel-file/node';
+import readXlsxFile from 'read-excel-file/node';
+import fs from 'node:fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-let filePath = 'files/output.xlsx';
 
+let filePath = fs.readFileSync('config.txt', { encoding: 'utf8' });
+let vehicles = [];
 
-class Report {
-    date;
-    vehicleNumber;
-    vin;
-    driver;
-    currentMileage;
-    lastOilChangeDate;
-    lastOilChangeMileage;
-    plateNumber;
-    renewalDate;
-    safetyinspectionDate;
-
-    constructor(args) {
-        this.date = new Date(args[0].split(':')[1].trim()).toDateString();
-        this.vehicleNumber = args[1].split(':')[1].trim();
-        this.vin = args[2].split(':')[1].trim();
-        this.driver = args[3].split(':')[1].trim();
-        this.currentMileage = parseInt(args[4].split(':')[1].replace('_', '').replace(',', '').trim());
-        this.lastOilChangeDate = new Date(args[5].split(':')[1].substring(0, 10).trim()).toDateString();
-        this.lastOilChangeMileage = parseInt(args[5].split(':')[2].replace(',', '').replace('_', '').trim());
-        this.plateNumber = args[6].split(':')[1].replace('Renewal Date', '').trim();
-        this.renewalDate = args[6].split(':')[2].trim();
-        this.safetyinspectionDate = args[7].split(':')[1].trim();
+const inSchema = {
+    'Vehicle Number': {
+        prop: 'vehicleNumber',
+        type: Number
+    },
+    'Starting Miles': {
+        prop: 'startingMiles',
+        type: Number
+    },
+    'January': {
+        prop: 'januaryMiles',
+        type: Number
+    },
+    'February': {
+        prop: 'februaryMiles',
+        type: Number
+    },
+    'March': {
+        prop: 'marchMiles',
+        type: Number
+    },
+    'April': {
+        prop: 'aprilMiles',
+        type: Number
+    },
+    'May': {
+        prop: 'mayMiles',
+        type: Number
+    },
+    'June': {
+        prop: 'juneMiles',
+        type: Number
+    },
+    'July': {
+        prop: 'julyMiles',
+        type: Number
+    },
+    'August': {
+        prop: 'augustMiles',
+        type: Number
+    },
+    'September': {
+        prop: 'septemberMiles',
+        type: Number
+    },
+    'October': {
+        prop: 'octoberMiles',
+        type: Number
+    },
+    'November': {
+        prop: 'novemberMiles',
+        type: Number
+    },
+    'December': {
+        prop: 'decemberMiles',
+        type: Number
+    },
+    'Total Yearly Miles': {
+        prop: 'totalYearlyMiles',
+        type: Number
+    },
+    'Ending Miles': {
+        prop: 'endingMiles',
+        type: Number
     }
 }
 
-const schema = [
-    {
-        column: 'Date',
-        type: String,
-        value: (report) => report.date
-    },
+const outSchema = [
     {
         column: 'Vehicle Number',
-        type: String,
-        value: (report) => report.vehicleNumber
-    },
-    {
-        column: 'VIN',
-        type: String,
-        value: (report) => report.vin
-    },
-    {
-        column: 'Driver',
-        type: String,
-        value: (report) => report.driver
-    },
-    {
-        column: 'Current Mileage',
         type: Number,
-        value: (report) => report.currentMileage
+        value: (vehicle) => vehicle.vehicleNumber
     },
     {
-        column: 'Last Oil Change Date',
-        type: String,
-        value: (report) => report.lastOilChangeDate
-    },
-    {
-        column: 'Last Oil Change Mileage',
+        column: 'Starting Miles',
         type: Number,
-        value: (report) => report.lastOilChangeMileage
+        value: (vehicle) => vehicle.startingMiles
     },
     {
-        column: 'Plate Number',
-        type: String,
-        value: (report) => report.plateNumber
+        column: 'January',
+        type: Number,
+        value: (vehicle) => vehicle.januaryMiles
     },
     {
-        column: 'Renewal Date',
-        type: String,
-        value: (report) => report.renewalDate
+        column: 'February',
+        type: Number,
+        value: (vehicle) => vehicle.februaryMiles
     },
     {
-        column: 'Safety Inspection Date',
-        type: String,
-        value: (report) => report.safetyinspectionDate
-    }
+        column: 'March',
+        type: Number,
+        value: (vehicle) => vehicle.marchMiles
+    },
+    {
+        column: 'April',
+        type: Number,
+        value: (vehicle) => vehicle.aprilMiles
+    },
+    {
+        column: 'May',
+        type: Number,
+        value: (vehicle) => vehicle.mayMiles
+    },
+    {
+        column: 'June',
+        type: Number,
+        value: (vehicle) => vehicle.juneMiles
+    },
+    {
+        column: 'July',
+        type: Number,
+        value: (vehicle) => vehicle.julyMiles
+    },
+    {
+        column: 'August',
+        type: Number,
+        value: (vehicle) => vehicle.augustMiles
+    },
+    {
+        column: 'September',
+        type: Number,
+        value: (vehicle) => vehicle.septemberMiles
+    },
+    {
+        column: 'October',
+        type: Number,
+        value: (vehicle) => vehicle.octoberMiles
+    },
+    {
+        column: 'November',
+        type: Number,
+        value: (vehicle) => vehicle.novemberMiles
+    },
+    {
+        column: 'December',
+        type: Number,
+        value: (vehicle) => vehicle.decemberMiles
+    },
+    {
+        column: 'Total Yearly Miles',
+        type: 'Formula',
+        value: (vehicle) => `SUM(C${vehicle.vehicleNumber - 100 + 1}:N${vehicle.vehicleNumber - 100 + 1})`
+    },
+    {
+        column: 'Ending Miles',
+        type: 'Formula',
+        value: (vehicle) => `SUM(B${vehicle.vehicleNumber - 100 + 1}:N${vehicle.vehicleNumber - 100 + 1})`
+    },
 ]
 
+const getXlsxData = async () => {
+    try {
+        const data = await readXlsxFile(filePath, {
+            schema: inSchema,
+            schemaPropertyValueForEmptyCell: null,
+            getEmptyObjectValue: () => null
+        });
+        for (const row of data.rows){
+            const vehicle = new Vehicle(row);
+            vehicles.push(vehicle);
+        }
+        console.log(vehicles[0])
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+if (filePath) getXlsxData(filePath);
+
+class Vehicle {
+    vehicleNumber;
+    startingMiles;
+    januaryMiles;
+    februaryMiles;
+    marchMiles;
+    aprilMiles;
+    mayMiles;
+    juneMiles;
+    julyMiles;
+    augustMiles;
+    septemberMiles;
+    octoberMiles;
+    novemberMiles;
+    decemberMiles;
+    totalYearlyMiles;
+    endingMiles;
+
+    // getTotalYearlyMiles() {
+    //     const milesArr = [
+    //         this.januaryMiles ? this.januaryMiles : null,
+    //         this.februaryMiles ? this.februaryMiles : null,
+    //         this.marchMiles ? this.marchMiles : null,
+    //         this.aprilMiles ? this.aprilMiles : null,
+    //         this.mayMiles ? this.mayMiles : null,
+    //         this.juneMiles ? this.juneMiles : null,
+    //         this.julyMiles ? this.julyMiles : null,
+    //         this.augustMiles ? this.augustMiles : null,
+    //         this.septemberMiles ? this.septemberMiles : null,
+    //         this.octoberMilesv ? this.octoberMiles : null,
+    //         this.novemberMiles ? this.novemberMiles : null,
+    //         this.decemberMiles ? this.decemberMiles : null,
+    //     ]
+    //     const total = milesArr.reduce((prev, curr) => { return prev ? prev += curr : curr }, 0);
+    //     return total;
+    // }
+
+    // calculateEndingMiles() {
+    //     return this.getTotalYearlyMiles() + this.startingMiles;
+    // }
+
+    constructor(row) {
+        this.vehicleNumber = row.vehicleNumber;
+        this.startingMiles = row.startingMiles;
+        this.januaryMiles = row.januaryMiles;
+        this.februaryMiles = row.februaryMiles;
+        this.marchMiles = row.marchMiles;
+        this.aprilMiles = row.aprilMiles;
+        this.mayMiles = row.mayMiles;
+        this.juneMiles = row.juneMiles;
+        this.julyMiles = row.julyMiles;
+        this.augustMiles = row.augustMiles;
+        this.septemberMiles = row.septemberMiles;
+        this.octoberMiles = row.octoberMiles;
+        this.novemberMiles = row.novemberMiles;
+        this.decemberMiles = row.decemberMiles;
+        this.totalYearlyMiles = row.totalYearlyMiles;
+        this.endingMiles = row.endingMiles;
+    }
+
+
+}
+
+class Report {
+    month;
+    vehicleNumber;
+    currentMileage;
+
+    constructor(args) {
+        const dateString = new Date(args[0].split(':')[1].trim()).toDateString();
+        this.month = 
+            dateString.includes('Jan') ? 'January' :
+            dateString.includes('Feb') ? 'February' :
+            dateString.includes('Mar') ? 'March' :
+            dateString.includes('Apr') ? 'April' :
+            dateString.includes('May') ? 'May' :
+            dateString.includes('Jun') ? 'June' :
+            dateString.includes('Jul') ? 'July' :
+            dateString.includes('Aug') ? 'August' :
+            dateString.includes('Sep') ? 'September' :
+            dateString.includes('Oct') ? 'October' :
+            dateString.includes('Nov') ? 'November' :
+            'December';
+        this.vehicleNumber = parseInt(args[1].split(':')[1].split(' ')[0].replace('#', '').trim());
+        this.currentMileage = parseInt(args[4].split(':')[1].replace('_', '').replace(',', '').trim());
+    }
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -115,6 +295,8 @@ app.on('window-all-closed', () => {
 ipcMain.handle('handle-doc', async (_, file) => {
     if (file.includes('.xlsx')) {
         filePath = file;
+        await getXlsxData(filePath);
+        fs.writeFile('config.txt', filePath, () => { console.log('Updated Config.txt') });
         console.log('Set excel sheet to: ', filePath)
     }
     else if (!file.includes('.docx')) {
@@ -125,8 +307,14 @@ ipcMain.handle('handle-doc', async (_, file) => {
         const docTextArr = docText.split('\n').filter(data => data.includes(":"));
         const report = new Report(docTextArr);
         console.log('Report read: ', report)
-        const objects = [report];
-        await writeXlsxFile(objects, { schema, filePath });
-        console.log('Excel Written!')
+        const objects = vehicles.map((vehicle)=>{
+            if (vehicle.vehicleNumber === report.vehicleNumber){
+                const key = report.month.toLowerCase().concat('Miles');
+                vehicle[key] = report.currentMileage - vehicle.endingMiles;
+            }
+            return vehicle;
+        });
+        await writeXlsxFile(objects, { schema: outSchema, filePath });
+        console.log(`Excel Written to ${filePath}!`)
     }
 });
